947c4cff1171cab6529e25ab160946d7f7f4a674
Rajdeep <ghoshrajdeep2000@gmail.com>
2020-11-13 20:01:12 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import styles from "./components/App.module.css";
 
 import { fetchData } from "./api";
 
+import coronaImage from "./images/image.png";
 class App extends React.Component {
   state = {
     country: "",
@@ -17,15 +18,17 @@ class App extends React.Component {
   }
 
   handleCountryChange = async (country) => {
-    console.log(country);
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
   };
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div>
         <div className={styles.container}>
+          <img className={styles.image} alt="covid-19" src={coronaImage} />
           <Cards data={data} />
-          <Charts data={data} />
+          <Charts data={data} country={country} />
           <CountryPicker handleCountryChange={this.handleCountryChange} />
         </div>
       </div>
--- src/api/index.js
+++ src/api/index.js
@@ -2,19 +2,26 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
-  
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+    console.log(changeableUrl);
+  }
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
     return { confirmed, recovered, deaths, lastUpdate };
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const fetchDailyData = async () => {
   try {
-    const { data } = await axios.get("https://covid19.mathdro.id/api/daily");
+    const { data } = await axios.get(`${url}/daily`);
     const modifiedData = data.map((dailyData) => ({
       confirmed: dailyData.confirmed.total,
       deaths: dailyData.deaths.total,
@@ -27,7 +34,7 @@ export const fetchDailyData = async () => {
 export const fetchCountries = async () => {
   try {
     const {
-      data: { countries }
+      data: { countries },
     } = await axios.get("https://covid19.mathdro.id/api/countries");
     return countries.map((country) => country.name);
   } catch (error) {
